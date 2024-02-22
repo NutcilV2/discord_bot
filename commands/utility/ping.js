910208248db7b3a5c03da9fd0aa0e3dd6a3e937e
Hunter Ford <gatorf2@gmail.com>
2024-02-22 17:47:26 -0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -6,5 +6,23 @@ module.exports = {
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
+
+    db.query('SELECT * FROM your_table LIMIT 1', (error, results) => {
+      if (error) throw error;
+
+      // Assuming 'results' is not empty and your table has columns you want to display
+      if (results.length > 0) {
+        const firstRow = results[0];
+        // Customize this message format as per your table structure
+        let replyMessage = `First Row:\n`;
+        Object.keys(firstRow).forEach(column => {
+          replyMessage += `${column}: ${firstRow[column]}\n`;
+        });
+
+        message.channel.send(replyMessage);
+      } else {
+        message.channel.send("No data found.");
+      }
+    });
 	},
 };
