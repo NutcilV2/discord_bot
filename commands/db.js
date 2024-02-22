36a6b176f7dd4e61f84202a380c06702dd3d5361
Hunter Ford <gatorf2@gmail.com>
2024-02-22 17:33:35 -0500

    added the db connection file

---
+++ commands/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const { config } = require('dotenv');
+
+
+config();
+const db_password = process.env.DISCORD_TOKEN;
+
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'discord',
+  password: db_password,
+  database: 'discord_db'
+});
+
+// Open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
