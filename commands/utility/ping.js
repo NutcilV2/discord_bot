51b1fa748c9fa7a6ee56223aa0d89e97863b44fc
Hunter Ford <gatorf2@gmail.com>
2024-02-25 14:16:09 -0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -18,10 +18,10 @@ module.exports = {
 
     try {
         const results = await queryPromise();
-				const eventString = results.map(item => `${item.Event_Id}: ${item.Event_Title}`).join('\n') + '\n';
+				const eventString = results.map(item => `    ${item.Event_Id}: ${item.Event_Title}`).join('\n') + '\n';
         // Ensure results are formatted in a way that can be sent in a message
         // For example, converting the results to a string or formatting them as needed
-        const replyMessage = `Events: ${eventString}`;
+        const replyMessage = `Events: \n${eventString}`;
         await interaction.reply(replyMessage);
     } catch (error) {
         console.error('An error occurred:', error);
