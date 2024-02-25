8f1923ee6b5fea6241f3851786d7c3fbda94c68c
Hunter Ford <gatorf2@gmail.com>
2024-02-25 14:14:50 -0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(interaction, connection) {
     // Convert connection.query to use Promises
     const queryPromise = () => new Promise((resolve, reject) => {
-        connection.query('SELECT Event_Title FROM events LIMIT 3', (error, results, fields) => {
+        connection.query('SELECT Event_Id, Event_Title FROM events LIMIT 3', (error, results, fields) => {
             if (error) {
                 reject(error);
             } else {
@@ -18,10 +18,10 @@ module.exports = {
 
     try {
         const results = await queryPromise();
-				const titlesString = results.map(item => item.Event_Title).join('\n');
+				const eventString = results.map(item => `${item.Event_Id}: ${item.Event_Title}`).join('\n') + '\n';
         // Ensure results are formatted in a way that can be sent in a message
         // For example, converting the results to a string or formatting them as needed
-        const replyMessage = `Events: ${titlesString}`;
+        const replyMessage = `Events: ${eventString}`;
         await interaction.reply(replyMessage);
     } catch (error) {
         console.error('An error occurred:', error);
