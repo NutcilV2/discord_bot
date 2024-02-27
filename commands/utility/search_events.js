cd8104b14b6a55e5b9c212107aefffadd11cfff9
Hunter Ford <gatorf2@gmail.com>
2024-02-27 13:14:34 -0500

    Update search_events.js

--- commands/utility/search_events.js
+++ commands/utility/search_events.js
@@ -111,7 +111,7 @@ module.exports = {
 				const eventString = results.map(item => `${item.Event_Id}: ${item.Event_Title} on ${item.Event_Date}`).join('\n') + '\n';
         // Ensure results are formatted in a way that can be sent in a message
         // For example, converting the results to a string or formatting them as needed
-        const replyMessage = `Events: \n${eventString}`;
+        const replyMessage = eventString ? `Events:\n${eventString}` : 'No upcoming events for that criteria.';
         await interaction.reply(replyMessage);
     } catch (error) {
         console.error('An error occurred:', error);
