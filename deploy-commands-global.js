4a61705ae0ea7c423e1607232d54e4dd6ff2dfa3
Hunter Ford <gatorf2@gmail.com>
2024-02-27 11:48:05 -0500

    global commands deployment

---
+++ deploy-commands-global.js
@@ -0,0 +1,43 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { config } = require('dotenv');
+
+config();
+const clientId = process.env.DISCORD_clientId;
+const token = process.env.DISCORD_TOKEN;
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath).filter(folder => folder !== 'scheduledCommands');
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const rest = new REST().setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands globally.`);
+
+        await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${commands.length} application (/) commands globally.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- deploy-commands-guild.js
+++ deploy-commands-guild.js
@@ -12,7 +12,7 @@ const commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath).filter(folder => folder !== 'scheduledCommands');
-//const commandFolders = fs.readdirSync(foldersPath);
+const commandFolders = fs.readdirSync(foldersPath);
 
 
 for (const folder of commandFolders) {
