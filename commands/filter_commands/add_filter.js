752e5b24cd3537dc592dfb712fadf27451cad4d1
Hunter Ford <gatorf2@gmail.com>
2024-02-27 10:56:08 -0500

    Update add_filter.js

--- commands/filter_commands/add_filter.js
+++ commands/filter_commands/add_filter.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('add_filter')
-        .setDescription('Removes specified filters from your list')
+        .setDescription('Adds specified filters from your list')
         .addStringOption(option =>
             option.setName('filter')
-                .setDescription('The filter(s) you want to remove, separated by commas')
+                .setDescription('The filter(s) you want to add, separated by commas')
                 .setRequired(true)), // Make sure the filter is required for command execution
     async execute(interaction, connection) {
         const user_id = interaction.user.id; // Get the user's ID
@@ -49,10 +49,10 @@ module.exports = {
             });
 
             await updateFilters(currentFiltersArray.join(',')); // Update the database with the new filters list
-            await interaction.reply(`The specified filter(s) have been removed.`);
+            await interaction.reply(`The specified filter(s) have been added.`);
         } catch (error) {
             console.error('An error occurred:', error);
-            await interaction.reply('An error occurred while removing your filter(s).');
+            await interaction.reply('An error occurred while adding your filter(s).');
         }
     },
 };
