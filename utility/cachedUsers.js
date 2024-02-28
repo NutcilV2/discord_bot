edf0a789f85699c290886efba04e76cf0a32d43e
Hunter Ford <gatorf2@gmail.com>
2024-02-28 00:12:33 -0500

    Update cachedUsers.js

--- utility/cachedUsers.js
+++ utility/cachedUsers.js
@@ -8,14 +8,9 @@ const query = 'SELECT User_Id FROM users';
 connection.query(query, (error, results, fields) => {
   if (error) throw error;
 
-  // Iterate over the results and add each user ID to the Set
   results.forEach(row => {
-    console.log(row.User_Id);
     userCache.add(row.User_Id);
   });
-
-  // Log the Set to see the user IDs
-  console.log(userCache);
 });
 
 
