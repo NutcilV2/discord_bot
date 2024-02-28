0b6755bc0728c52085827dc6cc3ff6e6e56a8cde
Hunter Ford <gatorf2@gmail.com>
2024-02-28 12:21:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.once(Events.ClientReady, readyClient => {
 	  console.log(`${TOKEN}`)
 
 		// Schedule a task to run at 14:00 every day
-    schedule.scheduleJob('* * * * *', function() {
+    schedule.scheduleJob('00 00 * * *', function() {
         console.log('Running scheduled Direct Message task...');
         scheduled_dm.execute(client, connection, cachedUsers, true);
     });
