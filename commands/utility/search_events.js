021a70c99a058504d626989d566afc3fbf44eff7
Hunter Ford <gatorf2@gmail.com>
2024-02-29 09:18:03 -0500

    Update search_events.js

--- commands/utility/search_events.js
+++ commands/utility/search_events.js
@@ -24,10 +24,7 @@ module.exports = {
 	async execute(interaction, connection, cachedUsers) {
     const user_id = interaction.user.id;
     const user_username = interaction.user.username;
-    const isCached = await cachedUsers.isUserCached(user_id, user_username);
-
-    const today = new Date();
-		let formattedDate = (today.getMonth() + 1).toString().padStart(2, '0') + '/' + today.getDate().toString().padStart(2, '0') + '/' + today.getFullYear();;
+    const isCached = await cachedUsers.isUserCached(user_id, user_username)
 
     const filter = interaction.options.getString('filter').replace(/'/g, "");
     const terms = mysqlFunctions.parseFilter(sanitizeInput(filter)).map(term => sanitizeInput(term)); // Remove quotes from terms
@@ -37,14 +34,16 @@ module.exports = {
     if(interaction.options.getString('count')) { count = interaction.options.getString('count'); }
 
 
-    let queryString = `SELECT Event_Id, Event_Title, Event_Date FROM events WHERE Event_Date = ?`;
-    if(likeConditions) { queryString += ` AND (${likeConditions.join(' OR ')})`; }
-    queryString += `LIMIT ${count}`
-
-
+    let queryString = `SELECT Event_Id, Event_Title, Event_Date FROM events `;
     if (interaction.options.getString('date')) {
-			formattedDate = mysqlFunctions.parseDate(interaction.options.getString('date'));
+  			const formattedDate = mysqlFunctions.parseDate(interaction.options.getString('date'));
+        queryString += `WHERE Event_Date = ?`
 		}
+    if(likeConditions) {
+        if (interaction.options.getString('date')) { queryString += ` AND ` }
+        queryString += `(${likeConditions.join(' OR ')})`;
+    }
+    queryString += ` LIMIT ${count}`
 
     try {
         const result = await mysqlFunctions.runQuery(queryString, formattedDate);
