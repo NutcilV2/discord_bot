f5e8725571e437328fb428e4e822e4634d6c5252
Hunter Ford <gatorf2@gmail.com>
2024-03-01 17:49:50 -0500

    Update get_server_events.js

--- commands/event_commands/get_server_events.js
+++ commands/event_commands/get_server_events.js
@@ -29,7 +29,7 @@ module.exports = {
             interaction.reply(`Your Server does not have a Prefix`);
             return;
         } else {
-            server_prefix = `[${server_prefix}] - %`;
+            server_prefix = `[${server_prefix}]  -  %`;
         }
 
 
