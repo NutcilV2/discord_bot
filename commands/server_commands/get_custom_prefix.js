bea3e2e94a28c6b834886fea3a0f54ac8fc1342b
Hunter Ford <gatorf2@gmail.com>
2024-02-29 23:53:35 -0500

    Create get_custom_prefix.js

---
+++ commands/server_commands/get_custom_prefix.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { sanitizeInput } = require('../../utility/inputSanitizer.js');
+const mysqlFunctions = require('../../utility/mysqlFunctions');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('set_custom_prefix')
+        .setDescription('Sets your servers prefix for all events')
+        .addStringOption(option =>
+            option.setName('prefix')
+                .setDescription('The Prefix that you want for events')
+                .setRequired(true) // This makes the parameter optional
+        ),
+    async execute(interaction, connection, cachedUsers) {
+      const user_id = interaction.user.id;
+			const user_username = interaction.user.username;
+      const guildId = interaction.guildId;
+      const isCached = await cachedUsers.isUserCached(user_id, user_username);
+      const prefix = sanitizeInput(interaction.options.getString('prefix'));
+
+			try {
+          const result = await mysqlFunctions.updateServerPrefix(guildId, prefix);
+					const replyMessage = `You've set your Servers Prefix`;
+					await interaction.reply(replyMessage);
+			} catch (error) {
+					console.error('An error occurred:', error);
+					await interaction.reply('An error occurred while updating your Servers Prefix.');
+			}
+    },
+};
