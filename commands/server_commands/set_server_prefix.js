43ed4cbbbbcc589af8602787abaf1ee2a037491d
Hunter Ford <gatorf2@gmail.com>
2024-03-01 17:31:47 -0500

    changed the name custom to server

--- commands/server_commands/set_server_prefix.js
+++ commands/server_commands/set_server_prefix.js
@@ -4,17 +4,23 @@ const mysqlFunctions = require('../../utility/mysqlFunctions');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('get_custom_prefix')
-        .setDescription('Sets your servers prefix for all events'),
+        .setName('set_server_prefix')
+        .setDescription('Sets your servers prefix for all events')
+        .addStringOption(option =>
+            option.setName('prefix')
+                .setDescription('The Prefix that you want for events')
+                .setRequired(true) // This makes the parameter optional
+        ),
     async execute(interaction, connection, cachedUsers) {
       const user_id = interaction.user.id;
 			const user_username = interaction.user.username;
       const guildId = interaction.guildId;
       const isCached = await cachedUsers.isUserCached(user_id, user_username);
+      const prefix = sanitizeInput(interaction.options.getString('prefix'));
 
 			try {
-          const result = await mysqlFunctions.getServerPrefix(guildId);
-					const replyMessage = `Servers Prefix: ${result[0].Server_Prefix}`;
+          const result = await mysqlFunctions.updateServerPrefix(guildId, prefix);
+					const replyMessage = `You've set your Servers Prefix`;
 					await interaction.reply(replyMessage);
 			} catch (error) {
 					console.error('An error occurred:', error);
