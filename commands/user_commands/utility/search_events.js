db93f21080ebac1a2490891e723bfd0215ed9863
Hunter Ford <gatorf2@gmail.com>
2024-03-05 17:53:13 -0500

    Update search_events.js

--- commands/user_commands/utility/search_events.js
+++ commands/user_commands/utility/search_events.js
@@ -50,10 +50,35 @@ module.exports = {
     try {
         const result = await mysqlFunctions.runQuery(queryString, formattedDate);
 				const eventString = result.map(item => `${item.Event_Id}: ${item.Event_Title} on ${item.Event_Date}`).join('\n');
+
+				const eventsArray = result.map(item => ({
+				    id: item.Event_Id,
+				    name: item.Event_Title,
+				    date: item.Event_Date
+				}));
+
+				// Create an array of just the ids
+				const idsArray = eventsArray.map(event => event.id);
+				const titlesArray = eventsArray.map(event => event.name);
+				const datesArray = eventsArray.map(event => event.date);
+
+				const idsArrayString    = idsArray.map(item    => `${item}`).join('\n');
+				const titlesArrayString = titlesArray.map(item => `${item}`).join('\n');
+				const datesArrayString  = datesArray.map(item  => `${item}`).join('\n');
+
+				let embedMessage = new EmbedBuilder();
+				embedMessage.setAuthor({ name: user_username, iconUrl: interaction.user.avatarURL()});
+
+				embedMessage.addFields({ name:`IDs`, value:idsArrayString, inline:true});
+				embedMessage.addFields({ name:`TITLEs`, value:titlesArrayString, inline:true});
+				embedMessage.addFields({ name:`DATEs`, value:datesArrayString, inline:true});
+
+
         // Ensure results are formatted in a way that can be sent in a message
         // For example, converting the results to a string or formatting them as needed
-        const replyMessage = eventString ? `Events:\n${eventString}` : 'No upcoming events for that criteria.';
-        await interaction.reply(replyMessage);
+        //const replyMessage = eventString ? `Events:\n${eventString}` : 'No upcoming events for that criteria.';
+        //await interaction.reply(replyMessage);
+				await interaction.reply({ embeds: [embedMessage] });
     } catch (error) {
         console.error('An error occurred:', error);
         await interaction.reply('An error occurred while fetching events.');
