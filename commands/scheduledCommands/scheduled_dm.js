c5df7b83e45472be31320e11a8f9f6107b1c6229
Hunter Ford <gatorf2@gmail.com>
2024-03-19 00:08:13 -0400

    Update scheduled_dm.js

--- commands/scheduledCommands/scheduled_dm.js
+++ commands/scheduledCommands/scheduled_dm.js
@@ -64,6 +64,7 @@ module.exports = {
                 }
 
                 if(unfilteredArrayList.length - idsArray.length > 0) {
+                    embedMessage.addFields({ name: '\u200B', value:`There are no events today`} ),
                     embedMessage.addFields({ name: '\u200B', value:`Other Events: ${unfilteredArrayList.length - idsArray.length}`} )
                 }
 
