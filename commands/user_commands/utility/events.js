d902bd9684d9e87e45e65059c0576768df48c24b
Hunter Ford <gatorf2@gmail.com>
2024-05-12 20:39:51 -0400

    Update events.js

--- commands/user_commands/utility/events.js
+++ commands/user_commands/utility/events.js
@@ -84,7 +84,7 @@ module.exports = {
     				let embedMessage = new EmbedBuilder();
             if(idsArrayString.length == 0) {
               embedMessage.setColor(colors.errorColor);
-              embedMessage.addFields({ name:`📢 **Alert: **`, value:`🚫 No Events for ${formattedDate}`, inline:false});
+              embedMessage.addFields({ name:`📢 #**Alert: **`, value:`🚫 No Events for ${formattedDate}`, inline:false});
             }
             else {
               embedMessage.setColor(colors.defaultColor);
