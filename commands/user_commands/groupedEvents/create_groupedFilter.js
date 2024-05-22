fba537d6ccfbd54e827856719f19bd99bad62730
Hunter Ford <gatorf2@gmail.com>
2024-05-22 16:17:16 -0400

    Create create_groupedFilter.js

---
+++ commands/user_commands/groupedEvents/create_groupedFilter.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { sanitizeInput } = require('../../../utility/inputSanitizer.js');
+const mysqlFunctions = require('../../../utility/mysqlFunctions');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('create_groupedFilter')
+        .setDescription('Adds specified filter(s) from your list')
+        .addStringOption(option =>
+            option.setName('filters')
+                .setDescription('The filters you want to add, separated by commas')
+                .setRequired(true)), // Make sure the filter is required for command execution
+    async execute(interaction, connection, cachedUsers) {
+        const user_id = interaction.user.id;
+        const user_username = interaction.user.username;
+        const isCached = await cachedUsers.isUserCached(user_id, user_username);
+        const rawInput = interaction.options.getString('filter');
+        let filtersToAdd = sanitizeInput(rawInput).split(',').map(filter => sanitizeInput(filter)); // Convert to array and trim whitespace
+
+        try {
+            const filterString = await mysqlFunctions.fetchUserFilters(user_id);
+            let currentFiltersArray = filterString.split(',').filter(Boolean).map(filter => sanitizeInput(filter)); // Split into array and remove any empty strings
+
+            // Remove each specified filter from the current filters array
+            filtersToAdd.forEach(filter => {
+                if (!currentFiltersArray.includes(filter)) {
+                    currentFiltersArray.push(filter); // Add the filter if not found
+                }
+            });
+
+            //await mysqlFunctions.updateUserFilter(user_id, user_username, currentFiltersArray.join(','));
+            await interaction.reply(`The specified filters have been turned into a group`);
+        } catch (error) {
+            console.error('An error occurred:', error);
+            await interaction.reply(`An error occurred while adding the specified filter(s) to the list.`);
+        }
+    },
+};
